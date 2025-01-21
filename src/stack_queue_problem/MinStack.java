ae05a711aebc7ec5b367fda900f7120d12e84381
Ngô Gia Thưởng <110913482+giathuong172@users.noreply.github.com>
2025-01-21 15:49:09 +0700

    stack and queue commit

---
+++ src/stack_queue_problem/MinStack.java
@@ -0,0 +1,44 @@
+package stack_queue_problem;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MinStack {
+    /*
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+     */
+    private Deque<Integer> stk1 = new ArrayDeque<>();
+    private Deque<Integer> stk2 = new ArrayDeque<>();
+
+    public MinStack() {
+        stk2.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stk1.push(val);
+        stk2.push(Math.min(val, stk2.peek()));
+    }
+
+    public void pop() {
+        stk1.pop();
+        stk2.pop();
+    }
+
+    public int top() {
+        return stk1.peek();
+    }
+
+    public int getMin() {
+        return stk2.peek();
+    }
+}
---
+++ src/stack_queue_problem/ValidParenthese.java
@@ -0,0 +1,57 @@
+package stack_queue_problem;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ValidParenthese {
+    /*
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+Constraints:
+ */
+    public boolean isValid(String s) {
+        Deque<Character> stk = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else if (stk.isEmpty() || !match(stk.pop(), c)) {
+                return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+
+    private boolean match(char l, char r) {
+        return (l == '(' && r == ')') || (l == '{' && r == '}') || (l == '[' && r == ']');
+    }
+}
